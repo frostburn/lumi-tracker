fa9483ee960116d3828b970a04f4b0cb3ee4e444
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-04-08 13:19:55 +0300

    Implement smitonic notation for 4L3s

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script>
 import Track from "./components/Track.vue";
-import { NOTE_OFF, mosMonzoToDiatonic } from "./util.js";
+import { NOTE_OFF, mosMonzoToDiatonic, mosMonzoToSmitonic } from "./util.js";
 
 export default {
   components: {
     Track,
   },
   data() {
+    /*
     return {
       mosPattern: "LsLLsLL",
       l: 2,
       s: 1,
-      tracks : [
+      tracks: [
         [
           {monzo: [0, 0], velocity: 0xFF},  // C3
           null,
@@ -38,6 +39,34 @@ export default {
         ]
       ],
     }
+    */
+    return {
+      mosPattern: "LsLsLsL",
+      l: 5,
+      s: 2,
+      tracks: [
+        [
+          {monzo: [0, 0], velocity: 0xFF},  // J3
+          {monzo: [1, 0], velocity: 0xFF},  // K3
+          {monzo: [1, 1], velocity: 0xFF},  // L3
+          {monzo: [2, 1], velocity: 0xFF},  // M3
+          {monzo: [2, 2], velocity: 0xFF},  // N3
+          {monzo: [3, 2], velocity: 0xFF},  // O3
+          {monzo: [3, 3], velocity: 0xFF},  // P3
+          {monzo: [4, 3], velocity: 0xFF},  // J4
+        ],
+        [
+          {monzo: [0, 1], velocity: 0x80},
+          {monzo: [1, -1], velocity: 0x80},
+          {monzo: [1, 2], velocity: 0x80},
+          {monzo: [2, -1], velocity: 0x80},
+          {monzo: [2, -2], velocity: 0x80},
+          {monzo: [3, 1], velocity: 0x80},
+          {monzo: [3, 0], velocity: 0x80},
+          {monzo: [3, 5], velocity: 0x80},
+        ],
+      ]
+    }
   },
   computed: {
     countL() {
@@ -50,8 +79,19 @@ export default {
       this.mosPattern.split("").forEach(c => c === "s" ? count++ : null);
       return count;
     },
+    mos() {
+      return this.countL + "L" + this.countS + "s";
+    },
     divisions() {
       return this.countL*this.l + this.countS*this.s;
+    },
+    notation() {
+      if (this.mos === "5L2s") {
+        return mosMonzoToDiatonic;
+      }
+      if (this.mos === "4L3s") {
+        return mosMonzoToSmitonic;
+      }
     },
     tracksWithNotes() {
       const result = [];
@@ -64,7 +104,7 @@ export default {
             return {note: "▭", velocity: NaN};
           }
           return {
-            note: mosMonzoToDiatonic(cell.monzo),
+            note: this.notation(cell.monzo),
             velocity: cell.velocity
           };
         }));
--- src/util.js
+++ src/util.js
@@ -9,8 +9,8 @@ export function mosMonzoToEtStep(monzo, l, s) {
     return coordL*l + coordS*s;
 }
 
-const LYDIAN = "FCGDAEB";
-const LYDIAN_INDEX_C = LYDIAN.indexOf("C");
+const FIFTHS_CHAIN = "FCGDAEB";
+const FIFTHS_CHAIN_INDEX_C = FIFTHS_CHAIN.indexOf("C");
 const REFERENCE_OCTAVE = 3;
 const SHARP_FIFTHS = 7;
 const SHARP_OCTAVES = -4;
@@ -22,28 +22,31 @@ export function mosMonzoToDiatonic(monzo) {
     const fifths = 2*coordL - 5*coordS;
     let octaves = 3*coordS - coordL + REFERENCE_OCTAVE;
 
-    let index = fifths + LYDIAN_INDEX_C;
-    octaves += OCTAVE_CORRECTIONS[mod(index, LYDIAN.length)];
-    const letter = LYDIAN[mod(index, LYDIAN.length)];
+    let index = fifths + FIFTHS_CHAIN_INDEX_C;
+
+    octaves += OCTAVE_CORRECTIONS[mod(index, FIFTHS_CHAIN.length)];
+
+    const letter = FIFTHS_CHAIN[mod(index, FIFTHS_CHAIN.length)];
     let accidental = "";
-    while (index < -LYDIAN.length) {
+
+    while (index < -FIFTHS_CHAIN.length) {
         accidental += "𝄫";
-        index += 2*LYDIAN.length;
+        index += 2*FIFTHS_CHAIN.length;
         octaves += 2*SHARP_OCTAVES;
     }
     while (index < 0) {
         accidental += "♭";
-        index += LYDIAN.length;
+        index += FIFTHS_CHAIN.length;
         octaves += SHARP_OCTAVES;
     }
-    while (index >= 2*LYDIAN.length) {
+    while (index >= 2*FIFTHS_CHAIN.length) {
         accidental += "𝄪";
-        index -= 2*LYDIAN.length;
+        index -= 2*FIFTHS_CHAIN.length;
         octaves -= 2*SHARP_OCTAVES;
     }
-    while (index >= LYDIAN.length) {
+    while (index >= FIFTHS_CHAIN.length) {
         accidental += "♯";
-        index -= LYDIAN.length;
+        index -= FIFTHS_CHAIN.length;
         octaves -= SHARP_OCTAVES;
     }
     if (accidental === "") {
@@ -52,4 +55,47 @@ export function mosMonzoToDiatonic(monzo) {
     return letter + accidental + octaves.toString(16);
 }
 
-// TODO: TAMNAMS J3 at [0, 0]
+// TODO: Generalize
+const SMI_CHAIN = "KMOJLNP"
+const SMI_CHAIN_INDEX_J = SMI_CHAIN.indexOf("J");
+const SMI_AMP_GENS = 7;
+const SMI_AMP_OCTAVES = -2;
+const SMI_CORRECTIONS = [-1, -1, -1, 0, 0, 0, 0];
+
+export function mosMonzoToSmitonic(monzo) {
+    const [coordL, coordS] = monzo;
+    const smithirds = 4*coordS - 3*coordL;
+    let octaves = coordL - coordS + REFERENCE_OCTAVE;
+
+    let index = smithirds + SMI_CHAIN_INDEX_J;
+
+    octaves += SMI_CORRECTIONS[mod(index, SMI_CHAIN.length)];
+
+    const letter = SMI_CHAIN[mod(index, SMI_CHAIN.length)];
+    let accidental = "";
+
+    while (index < -SMI_CHAIN.length) {
+        accidental += "§";  // Non-standard
+        index += 2*SMI_CHAIN.length;
+        octaves += 2*SMI_AMP_OCTAVES;
+    }
+    while (index < 0) {
+        accidental += "&";
+        index += SMI_CHAIN.length;
+        octaves += SMI_AMP_OCTAVES;
+    }
+    while (index >= 2*SMI_CHAIN.length) {
+        accidental += "©";  // Non-standard
+        index -= 2*SMI_CHAIN.length;
+        octaves -= 2*SMI_AMP_OCTAVES;
+    }
+    while (index >= SMI_CHAIN.length) {
+        accidental += "@";
+        index -= SMI_CHAIN.length;
+        octaves -= SMI_AMP_OCTAVES;
+    }
+    if (accidental === "") {
+        accidental = "·";  // Non-standard
+    }
+    return letter + accidental + octaves.toString(16);
+}
